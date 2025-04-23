8fadc06fd1aca6c2ad6e8a0ad6d9fae49f4f8fe7
avigupta63 <avigupta@DESKTOP-O1AMAE3.>
2025-04-23 06:16:42 +0000

    sonarcube

--- declpipeline.groovy
+++ declpipeline.groovy
@@ -15,7 +15,7 @@ pipeline {
         }
         stage('test') {
             steps {
-                sh 'opt/maven/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp -Dsonar.host.url=http://54.151.132.53:9000 -Dsonar.login=8c1e2f756c0df03d5d06af5776e175a38b7d07d1'
+                sh '/opt/maven/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp -Dsonar.host.url=http://54.151.132.53:9000 -Dsonar.login=8c1e2f756c0df03d5d06af5776e175a38b7d07d1'
                 //
             }
         }    
